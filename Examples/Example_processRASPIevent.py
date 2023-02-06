0e5a79414c67de84d7fe87c27a28d42d205fc2e5
hkunwer <114240436+hkunwer@users.noreply.github.com>
2023-02-06 11:12:00 -0500

    test raspi example

---
+++ Examples/Example_processRASPIevent.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+program to get event-based waveform data around theoretical P-arrival time
+from the NEIC server, and including instrument response removal from IRIS
+to the traces, the following informaiton is added:
+Stations currently default to the GSN network
+ Andrew Newman  Fri Oct 29 12:35:11 EDT 2021
+"""
+from rtergpy.run import defaults, event, etime2name, src2ergs
+from obspy import UTCDateTime
+
+Defaults=defaults()
+Event=event()
+
+Defaults.src='RASPISHAKE'
+# 2023 Turkey EQ (Feb 6, 2023 - mainshock)
+eloc = [37.17,37.03,17.9]
+etime= UTCDateTime(2023,2,6,1,17,35) 
+Event.ecount='00'
+#Event.newData=False   # use already downloaded data
+Event.newData=True
+Event.eventname=etime2name(etime,ecount=Event.ecount)
+Event.origin=[eloc,etime]
+Event.focmech=[318, 89, -179] # phi,delta,lmbda
+
+print(Event.eventname)
+src2ergs(Defaults=Defaults,Event=Event)  # need to export run output in a coherent way
