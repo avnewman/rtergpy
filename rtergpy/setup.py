137ac08184216d5fd21093c0b7e19a3fce64df89
Andrew Newman <andrewvnewman@gmail.com>
2025-08-05 19:46:35 -0400

    version update.  initial seismo-geodetic data integration

--- rtergpy/setup.py
+++ rtergpy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rtergpy',
-    version='0.4.0',
+    version='0.4.1',
     description='A package for real-time processing of Earthquake Energy and duration',
     url='https://github.com/avnewman/',
     author='Andrew Newman',
@@ -13,6 +13,6 @@ setup(
         'Development Status :: 2 - Beta',
         'Intended Audience :: Science',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :3.10',
+        'Programming Language :: Python :: 3.11',
         ],
 )
